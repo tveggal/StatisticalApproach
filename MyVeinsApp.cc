99c6760f5d82c43d5cf08e53452a18e3d5efc834
thulasi <19488277+tveggal@users.noreply.github.com>
2026-03-04 14:44:20 -0600

    Update MyVeinsApp.cc

--- MyVeinsApp.cc
+++ MyVeinsApp.cc
@@ -1,121 +1,140 @@
-#include "veins/modules/application/traci/MyVeinsApp.h"
+#include "MyVeinsApp.h"
+#include <cmath>
 
-using namespace veins;
-
-Define_Module(veins::MyVeinsApp);
+Define_Module(MyVeinsApp);
 
 void MyVeinsApp::initialize(int stage)
 {
-    DemoBaseApplLayer::initialize(stage);
+    BaseWaveApplLayer::initialize(stage);
 
     if (stage == 0) {
+        // Attack assignment
+        isAttacker = uniform(0, 1) < 0.10;        // 10% vehicles are attackers
+        attackType = intuniform(1,5);             // pick attack type randomly
 
-        predictionInterval = 0.3;
-        speedOfLight = 3e8;
+        falseOffset = par("falseOffset").doubleValue();
 
-        propagationThreshold = 20.0;
-        positionThreshold = 10.0;
+        predictionInterval = 0.3;      // 300 ms
+        positionThreshold = 5.0;       // meters
+        propagationThreshold = 10.0;   // meters
+        speedOfLight = 3e8;            // m/s
 
-        TP = FP = TN = FN = 0;
-
-        logFile.open("veins_dataset.csv");
-
-        logFile << "senderId,"
-                << "distance_sr,"
-                << "deltaTime,"
-                << "propagationError,"
-                << "predictionError,"
-                << "label\n";
+        // open CSV for logging
+        logFile.open("bsm_features.csv");
+        logFile << "senderId,distance_sr,predicted_error,propagation_error,detectedMalicious\n";
     }
-}
-
-void MyVeinsApp::onBSM(DemoSafetyMessage* bsm)
-{
-    int senderId = bsm->getSenderAddress();
-
-    Coord senderPos = bsm->getSenderPos();
-    Coord senderSpeed = bsm->getSenderSpeed();
-    simtime_t t1 = bsm->getTimestamp();
-    simtime_t t = simTime();
-
-    Coord receiverPos = mobility->getCurrentPosition();
-
-    double deltaTime = (t - t1).dbl();
-
-    // Feature 1: sender-receiver distance
-    double distance_sr =
-        sqrt(pow(senderPos.x - receiverPos.x, 2) +
-             pow(senderPos.y - receiverPos.y, 2));
-
-    // Feature 2: propagation error
-    double expectedDistance = speedOfLight * deltaTime;
-    double propagationError =
-        fabs(distance_sr - expectedDistance);
-
-    // Feature 3: motion prediction error
-    double predictionError = 0;
-
-    if (lastBeacon.find(senderId) != lastBeacon.end()) {
-
-        BeaconInfo prev = lastBeacon[senderId];
-
-        Coord predicted;
-        predicted.x = prev.position.x +
-                      prev.speed.x * predictionInterval;
-        predicted.y = prev.position.y +
-                      prev.speed.y * predictionInterval;
-
-        predictionError =
-            sqrt(pow(predicted.x - senderPos.x, 2) +
-                 pow(predicted.y - senderPos.y, 2));
-    }
-
-    bool detectedMalicious =
-        (propagationError > propagationThreshold) ||
-        (predictionError > positionThreshold);
-
-    // Example ground truth
-    bool actualMalicious = false;
-    if (senderId % 10 == 0)
-        actualMalicious = true;
-
-    if (detectedMalicious && actualMalicious) TP++;
-    else if (detectedMalicious && !actualMalicious) FP++;
-    else if (!detectedMalicious && !actualMalicious) TN++;
-    else if (!detectedMalicious && actualMalicious) FN++;
-
-    // LOG TO CSV
-    logFile << senderId << ","
-            << distance_sr << ","
-            << deltaTime << ","
-            << propagationError << ","
-            << predictionError << ","
-            << actualMalicious << "\n";
-
-    BeaconInfo info;
-    info.position = senderPos;
-    info.speed = senderSpeed;
-    info.timestamp = t1;
-
-    lastBeacon[senderId] = info;
 }
 
 void MyVeinsApp::finish()
 {
-    double accuracy =
-        (double)(TP + TN) / (TP + TN + FP + FN);
+    if (logFile.is_open()) {
+        logFile.close();
+    }
+    BaseWaveApplLayer::finish();
+}
 
-    double precision =
-        (TP + FP) ? (double)TP / (TP + FP) : 0;
+void MyVeinsApp::populateWSM(BaseFrame1609_4* wsm)
+{
+    Coord realPos = mobility->getCurrentPosition();
+    Coord speed = mobility->getCurrentSpeed();
 
-    double recall =
-        (TP + FN) ? (double)TP / (TP + FN) : 0;
+    Coord transmitPos = realPos;
 
-    EV << "Accuracy: " << accuracy << endl;
-    EV << "Precision: " << precision << endl;
-    EV << "Recall: " << recall << endl;
+    if (isAttacker) {
+        switch (attackType) {
+            case 1: // constant position
+                transmitPos = Coord(1000, 1000, 0); 
+                break;
+            case 2: // constant offset
+                transmitPos.x += falseOffset;
+                transmitPos.y += falseOffset;
+                break;
+            case 3: // random
+                transmitPos.x = uniform(0, 1000);
+                transmitPos.y = uniform(0, 1000);
+                break;
+            case 4: // random offset
+                transmitPos.x += uniform(-falseOffset, falseOffset);
+                transmitPos.y += uniform(-falseOffset, falseOffset);
+                break;
+            case 5: // eventual stop
+                if (simTime() > 10) { // starts attack after 10s
+                    transmitPos = lastBeacon[wsm->getSenderAddress()].position;
+                }
+                break;
+        }
+    }
 
-    logFile.close();
+    wsm->setSenderPos(transmitPos);
+    wsm->setSenderSpeed(speed);
+    wsm->setTimestamp(simTime());
+}
 
-    DemoBaseApplLayer::finish();
+void MyVeinsApp::onWSM(BaseFrame1609_4* wsm)
+{
+    int senderId = wsm->getSenderAddress();
+    Coord senderPos = wsm->getSenderPos();
+    Coord senderSpeed = wsm->getSenderSpeed();
+    simtime_t t1 = wsm->getTimestamp();
+    simtime_t t = simTime();
+    Coord receiverPos = mobility->getCurrentPosition();
+
+    // ============================
+    // STEP 1: Propagation Validation
+    // ============================
+    double propagationError = computePropagationError(senderPos, receiverPos, t - t1);
+
+    // ============================
+    // STEP 2: Motion Prediction
+    // ============================
+    double predictionError = 0.0;
+    if (lastBeacon.find(senderId) != lastBeacon.end()) {
+        Coord predicted = computePredictedPosition(
+            lastBeacon[senderId].position,
+            lastBeacon[senderId].speed
+        );
+        predictionError = computeDistance(predicted, senderPos);
+    }
+
+    // Detect suspicious
+    bool detectedMalicious = (propagationError > propagationThreshold) || 
+                             (predictionError > positionThreshold);
+
+    // Log for ML phase
+    logFile << senderId << ","
+            << computeDistance(senderPos, receiverPos) << ","
+            << predictionError << ","
+            << propagationError << ","
+            << detectedMalicious << "\n";
+
+    // Store last beacon for next prediction
+    BeaconInfo info;
+    info.position = senderPos;
+    info.speed = senderSpeed;
+    info.timestamp = t1;
+    lastBeacon[senderId] = info;
+}
+
+// ============================
+// Helper functions
+// ============================
+double MyVeinsApp::computeDistance(Coord a, Coord b)
+{
+    return sqrt(pow(a.x - b.x,2) + pow(a.y - b.y,2));
+}
+
+Coord MyVeinsApp::computePredictedPosition(Coord pos, Coord speed)
+{
+    Coord predicted;
+    predicted.x = pos.x + speed.x * predictionInterval;
+    predicted.y = pos.y + speed.y * predictionInterval;
+    predicted.z = 0;
+    return predicted;
+}
+
+double MyVeinsApp::computePropagationError(Coord senderPos, Coord receiverPos, simtime_t delta)
+{
+    double actualDistance = computeDistance(senderPos, receiverPos);
+    double expectedDistance = speedOfLight * delta.dbl();
+    return fabs(actualDistance - expectedDistance);
 }
